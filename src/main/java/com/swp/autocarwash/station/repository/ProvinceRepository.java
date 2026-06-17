c0f2059d08974c5e774c733bd2e667af65b1ebf5
phongNiro <phongpor1234@gmail.com>
2026-06-17 16:56:27 +0700

    feat(station): implement get provinces API

---
+++ src/main/java/com/swp/autocarwash/station/repository/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package com.swp.autocarwash.station.repository;
+
+import com.swp.autocarwash.station.entity.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProvinceRepository extends JpaRepository<Province, Integer>{
+
+}
