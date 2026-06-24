4f4957267def3600848333dfb84b6246102242df
tramminhhieu <tramminhhieu09112005@gmail.com>
2026-06-24 13:16:15 +0700

    feat(customer): add vehicle registration API v2

--- src/main/java/com/swp/autocarwash/common/config/SecurityConfig.java
+++ src/main/java/com/swp/autocarwash/common/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
-
         return http.build();
     }
 }
