5be404ce51165daa11e65981c97e467f7db6442d
KimNgan <lekikingan@gmail.com>
2026-06-25 12:28:15 +0700

    feat(queue): create empty class for websocket

---
+++ src/main/java/com/swp/autocarwash/queue/websocket/publisher/QueueWebSocketPublisher.java
@@ -0,0 +1,17 @@
+package com.swp.autocarwash.queue.websocket.publisher;
+
+
+
+
+
+/**
+ * Chức năng: load trạng thái hàng chờ mới nhất của 1 station qua WebSocket,
+ * để Queue Dashboard của FE cập nhật real-time
+ *
+ * <p>Client subscribe: {@code /topic/queue/{stationId}}.</p>
+ *
+ * @author KimNgan
+ * @version 1.0
+ */
+public class QueueWebSocketPublisher {
+}
