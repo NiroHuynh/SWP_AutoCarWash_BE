55be917dca1e0be2a52b6be477134439a16b9b41
Nguyen Viet Binh <nguyenvietbinh26112005@gmail.com>
2026-06-25 23:20:52 +0700

    fea(queue_checkin) modify ScanVehicleResponse

--- src/main/java/com/swp/autocarwash/payment/repository/custom/BookingInvoiceRepository.java
+++ src/main/java/com/swp/autocarwash/payment/repository/custom/BookingInvoiceRepository.java
@@ -3,5 +3,9 @@ package com.swp.autocarwash.payment.repository.custom;
 import com.swp.autocarwash.payment.entity.BookingInvoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookingInvoiceRepository extends JpaRepository<BookingInvoice,Long> {
+    Optional<BookingInvoice> findByBooking_Id(Long bookingId);
+    //Optional<BookingInvoice> findByBookingId
 }
