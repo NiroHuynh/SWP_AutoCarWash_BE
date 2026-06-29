db0d2611850c9a288b687c9b185fd031311fe086
phongNiro <phongpor1234@gmail.com>
2026-06-29 10:59:41 +0700

    fix(booking): validate vehicle booking by slot date

--- src/main/java/com/swp/autocarwash/booking/validator/BookingValidator.java
+++ src/main/java/com/swp/autocarwash/booking/validator/BookingValidator.java
@@ -105,10 +105,13 @@ public class BookingValidator {
             List<Long> slotIds
     ) {
 //        kiểm tra hết hạn
-        validateSlotAlreadyBooked(vehicleId,slotIds);
+        validateSlotNotExpired(slotIds);
 
-//        kiểm tra đã booking slot này trước đó chưa
-        validateSlotAlreadyBooked(vehicleId, slotIds);
+////        kiểm tra đã booking slot này trước đó chưa
+//        validateSlotAlreadyBooked(vehicleId, slotIds);
+
+//        kiểm tra xem vehicle đã booking vào hôm nay chưa
+        validateVehicleBookedInSlotDate(vehicleId,slotIds);
 
         List<BookingSlot> slots =
                 slotRepository.findByIdIn(slotIds);
@@ -151,6 +154,35 @@ public class BookingValidator {
             );
         }
     }
+
+    private void validateVehicleBookedInSlotDate(
+            Long vehicleId,
+            List<Long> slotIds
+    ) {
+
+        List<BookingSlot> slots =
+                bookingSlotRepository.findAllById(slotIds);
+
+
+        for (BookingSlot slot : slots) {
+
+            boolean exists =
+                    bookingSlotAllocationRepository
+                            .existsVehicleBookedOnSlotDate(
+                                    vehicleId,
+                                    slot.getDate()
+                            );
+
+            if (exists) {
+                throw new BusinessException(
+                        ErrorCode.VEHICLE_ALREADY_BOOKED_TODAY
+                );
+            }
+        }
+    }
+
+
+
 
     private void validateSlotAlreadyBooked(
             Long vehicleId,
