5cab92047bfa3c2415149f18f1c93e9adb908e31
KimNgan <lekikingan@gmail.com>
2026-07-04 23:30:42 +0700

    feat(loyalty): add tier logic

---
+++ src/main/java/com/swp/autocarwash/loyalty/repository/custom/TierBenefitRepository.java
@@ -0,0 +1,12 @@
+package com.swp.autocarwash.loyalty.repository.custom;
+
+import com.swp.autocarwash.loyalty.entity.TierBenefit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TierBenefitRepository extends JpaRepository<TierBenefit, Integer> {
+
+    /** Danh sach quyen loi cua 1 hang thanh vien, theo thu tu id tang dan. */
+    List<TierBenefit> findByCustomerTier_IdOrderByIdAsc(Integer customerTierId);
+}
