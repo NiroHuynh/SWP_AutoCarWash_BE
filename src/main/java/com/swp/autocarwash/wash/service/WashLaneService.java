135f7c9e9a35ed7c7c34c0b1b6e0d0c20135a63b
Nguyen Viet Binh <nguyenvietbinh26112005@gmail.com>
2026-07-06 22:02:21 +0700

    fea(admin delete washlane)

--- src/main/java/com/swp/autocarwash/wash/service/WashLaneService.java
+++ src/main/java/com/swp/autocarwash/wash/service/WashLaneService.java
@@ -2,7 +2,12 @@ package com.swp.autocarwash.wash.service;
 
 import com.swp.autocarwash.wash.dto.request.CreateWashLaneRequest;
 import com.swp.autocarwash.wash.dto.response.CreateWashLaneResponse;
+import com.swp.autocarwash.wash.dto.response.WashLaneResponse;
+
+import java.util.List;
 
 public interface WashLaneService {
     CreateWashLaneResponse createWashLane(CreateWashLaneRequest request);
+    List<WashLaneResponse> getLanesByStation(Integer stationId);
+    void deleteWashLane(Integer laneId);
 }
